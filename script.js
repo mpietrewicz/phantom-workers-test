3f162b3a8ad305bc56e6c6ce1aca804e1568268c
Marek <mpietrewicz@gmail.com>
2017-10-29 11:22:23 +0100

    Inject local js (jquery) library

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ var port = system.env['PHANTOM_WORKER_PORT'];
 
 require('webserver').create().listen(port, function (req, res) {
 	console.log(host+':'+port);
-	//standard phantomjs script which get input parametrs from request
+	var url = "https://allegro.pl/kategoria/seria-3-e46-1998-2007-18077";
 	var page = require('webpage').create();
 	page.open(JSON.parse(req.post).url, function(status) {
+		page.injectJs("jquery.min.js");
     var title = page.evaluate(function() {
 	    return document.title;
 		});
