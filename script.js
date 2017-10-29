f3f65ff13be2675df9de22f0ecc5209dc022f249
Marek <mpietrewicz@gmail.com>
2017-10-29 11:42:26 +0100

    Jquery selectors test

--- script.js
+++ script.js
@@ -9,14 +9,27 @@ require('webserver').create().listen(port, function (req, res) {
 	var page = require('webpage').create();
 	page.open(url, function(status) {
 		page.injectJs("jquery.min.js");
-    var title = page.evaluate(function() {
-	    return document.title;
+    // var title = page.evaluate(function() {
+	  //   return document.title;
+		// });
+
+		var content = page.evaluate(function(){
+
+			var text = [];
+			$("h2:contains('lista promowanych ofert')").parents("section").find("article").each(function()
+			{
+				text.push($(this).find("h2 a").text());
+				text.push($(this).find("h2 a").attr("href"));
+				text.push($(this).find("div div span span:contains(' zł')").text());
+			});
+
+			return text;
 		});
 
 		//write the result to the response
 		res.statusCode = 200;
 		res.setHeader('Content-Type', 'application/json');
-	  res.write(JSON.stringify({ title: title }));
+	  res.write(JSON.stringify({ content: content }));
 	  res.close();
 	});
 });
