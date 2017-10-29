7fd0ff40b3795aab8cf6d734d2fabf238651ed32
Marek <mpietrewicz@gmail.com>
2017-10-29 11:48:43 +0100

    Page open get req.post.url

--- script.js
+++ script.js
@@ -5,9 +5,8 @@ var port = system.env['PHANTOM_WORKER_PORT'];
 
 require('webserver').create().listen(port, function (req, res) {
 	console.log(host+':'+port);
-	var url = "https://allegro.pl/kategoria/seria-3-e46-1998-2007-18077";
 	var page = require('webpage').create();
-	page.open(url, function(status) {
+	page.open(req.post.url, function(status) {
 		page.injectJs("jquery.min.js");
     // var title = page.evaluate(function() {
 	  //   return document.title;
